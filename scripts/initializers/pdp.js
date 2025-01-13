6c66c1af516e8d77116c2f2c79bb3b025de84bee
Stephen Rugh <rugh@adobe.com>
2025-01-13 08:31:21 -0600

    functional pdp
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- scripts/initializers/pdp.js
+++ scripts/initializers/pdp.js
@@ -24,15 +24,57 @@ export const IMAGES_SIZES = {
   height: 1191,
 };
 
+/** Temp function copied almost wholesale from for-stephen branch of storefront-search-dropin */
+function getMake() {
+  const context = JSON.parse(window.localStorage.getItem('_demo:context'));
+  const data = JSON.parse(window.localStorage.getItem('_demo:data'));
+  const makeId = context?.id;
+  let make;
+  const index = Object.keys(data).findIndex((key) => data[key].id === makeId);
+  if (index !== -1) {
+    make = Object.keys(data)[index];
+  }
+  return { make, makeId };
+}
+
+/** Temp function copied almost wholesale from for-stephen branch of storefront-search-dropin */
+function getPriceBookHeader() {
+  // will be Brand, Vip, or Employee
+  const priceBook = window.localStorage.getItem('_demo:price-book') || 'Brand';
+  if (priceBook === 'Brand') {
+    const context = JSON.parse(window.localStorage.getItem('_demo:context'));
+    return context?.value.toLowerCase();
+  }
+
+  return priceBook.toLowerCase();
+}
+
+/** Temp function copied almost wholesale from for-stephen branch of storefront-search-dropin */
+const getHeaders = () => {
+  const model = window.localStorage.getItem('_demo:model');
+  const { makeId } = getMake();
+  const priceBook = getPriceBookHeader();
+
+  const newHeaders = {
+    'Content-Type': 'application/json',
+    'AC-Environment-Id': 'c09ecfb1-333a-4db3-9669-ce07ec0e3a00',
+    ...(makeId ? { 'AC-Channel-Id': makeId } : {}),
+    ...(model ? { 'AC-Policy-Model': model } : {}),
+    ...(priceBook ? { 'AC-Price-Book-Id': priceBook } : {}),
+    'AC-Scope-Locale': 'en-US',
+    'X-Api-Key': 'search_gql',
+    'X-Request-Id': crypto.randomUUID(),
+  };
+  console.log('PDP Headers', JSON.stringify(newHeaders, null, 2));
+  return newHeaders;
+};
+
 await initializeDropin(async () => {
   // Set Fetch Endpoint (Service)
   setEndpoint(await commerceEndpointWithQueryParams());
 
   // Set Fetch Headers (Service)
-  setFetchGraphQlHeaders({
-    'Content-Type': 'application/json',
-    'x-api-key': await getConfigValue('commerce-x-api-key'),
-  });
+  setFetchGraphQlHeaders(getHeaders());
 
   const sku = getSkuFromUrl();
   const optionsUIDs = getOptionsUIDsFromUrl();
